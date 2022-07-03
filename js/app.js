ff5845e8d1a2634c0bb9679bee07f95613a6c298
yeonghoon Park <huklmnj2@naver.com>
2022-07-03 19:14:41 +0900

    canvas

--- js/app.js
+++ js/app.js
@@ -1,2 +1,44 @@
 'use strict';
 
+const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 600;
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+const stopPainting = ()=>{
+    painting = false;
+};
+
+const startPainting = ()=>{
+    painting = true;
+};
+
+const onMouseMove = (event)=>{
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+};
+
+const onMouseDown = (event)=>{
+    painting = true;
+};
+
+
+if(canvas){
+    canvas.addEventListener('mousemove', onMouseMove);
+    canvas.addEventListener('mousedown', startPainting);
+    canvas.addEventListener('mouseup', stopPainting);
+    canvas.addEventListener('mouseleave', stopPainting);
+}
